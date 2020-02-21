accd0e2b95ec84e7c10441ae2bc7ee0cff07bfec
Felipe Azevedo <felipe.azevedo@adidas.com>
2020-02-21 15:30:55 +0100

    Changing colors

--- src/container/Materials.js
+++ src/container/Materials.js
@@ -2,6 +2,6 @@ import './Materials.scss';
 
 import React from 'react';
 
-const Materials = () => <div id="materials">...</div>;
+const Materials = () => <div id="materials">Materials !</div>;
 
 export default Materials;
